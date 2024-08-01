11e884379b19ff76e897995a2d16ad844f1f67cb
wahyuPM <wahyupratama191@gmail.com>
2024-08-01 09:56:07 +0800

    add : bullet type with tooltip add : dynamic color bullet

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,27 +5,28 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
+export const baseColors: { [key: string]: string } = {
+  grass: '#5dbe6299',
+  fire: '#fc6c6d99',
+  water: '#60a5fa99',
+  bug: '#9dc13099',
+  flying: '#9bb4e899',
+  normal: '#9a9da180',
+  poison: '#b563ce99',
+  electric: '#edd53f99',
+  ground: '#d7855599',
+  fairy: '#ef97e680',
+  fighting: '#d9425699',
+  psychic: '#f8588899',
+  rock: '#c3b16299',
+  ghost: '#7975d499',
+  ice: '#98d8d880',
+  dragon: '#0773c799',
+  dark: '#5f606d99',
+  steel: '#b8b8d080'
+};
+
 export const getGradientStyle = (types: { type: { name: string } }[]): React.CSSProperties => {
-  const baseColors: { [key: string]: string } = {
-    grass: '#5dbe6299',
-    fire: '#fc6c6d99',
-    water: '#60a5fa99',
-    bug: '#9dc13099',
-    flying: '#9bb4e899',
-    normal: '#9a9da180',
-    poison: '#b563ce99',
-    electric: '#edd53f99',
-    ground: '#d7855599',
-    fairy: '#ef97e680',
-    fighting: '#d9425699',
-    psychic: '#f8588899',
-    rock: '#c3b16299',
-    ghost: '#7975d499',
-    ice: '#98d8d880',
-    dragon: '#0773c799',
-    dark: '#5f606d99',
-    steel: '#b8b8d080'
-  };
 
   if (types.length === 1) {
     const typeName = types[0].type.name;
